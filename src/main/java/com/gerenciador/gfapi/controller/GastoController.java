cd64adde98aaa47b093d965d1616372c65cf2e4e
Leonardo Silva <81488877+leonardoprogrammer@users.noreply.github.com>
2022-12-18 00:39:17 -0300

    Tratando erros

--- src/main/java/com/gerenciador/gfapi/controller/GastoController.java
+++ src/main/java/com/gerenciador/gfapi/controller/GastoController.java
@@ -40,7 +40,15 @@ public class GastoController {
     }
 
     @GetMapping
-    public ResponseEntity<Object> enviarGastosPorUsuario(@RequestParam(name = "usuario") long idUsuario) {
+    public ResponseEntity<Object> enviarGastosPorUsuario(@RequestParam(name = "usuario") String id) {
+        id = id.replaceAll("[^0-9]", "");
+
+        if (id.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: o id do usuário deve ser numérico.");
+        }
+
+        Long idUsuario = Long.parseLong(id);
+
         return ResponseEntity.status(HttpStatus.OK).body(gastoService.findByIdUsuario(idUsuario));
     }
 }
